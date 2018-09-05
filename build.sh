31ce6a6707c799ece542109f9060ecc22093007d
n-miyamoto <scotsdowneroad@gmail.com>
2018-09-05 01:20:41 +0000

    fix testcode and build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-gcc -c main.c -march=armv5
-gcc -c fft_arm9e.s -march=armv5
-gcc -o out main.o fft_arm9e.o -march=armv5
+gcc -c main.c -march=armv5te -marm
+gcc -c fft_arm9e.s -march=armv5te -marm 
+gcc -o out main.o fft_arm9e.o -march=armv5te -marm
 ./out
--- main.c
+++ main.c
@@ -3,19 +3,27 @@
 
 #include "fft_arm9e.h"
 
-#define N 16
+#define N 4
 
 int main(void){
-	
+	int i=0;	
 	short x[2*N];
 	short y[2*N];
 	
 	memset(x,0,4*N);
 	memset(y,0,4*N);
 
-	fft_16_arm9e(x, y, N);
+	for(i=0;i<N;i++){
+		x[2*i] = 1;
+		x[2*i+1]=0;
+	}
+	fft_16_arm9e(y, x, N);
 
-	for(int i=0;i<N;i++){
+	for(i=0;i<N;i++){
+		printf("%d + %d i \n", x[2*i], x[2*i+1]);
+	}
+
+	for(i=0;i<N;i++){
 		printf("%d + %d i \n", y[2*i], y[2*i+1]);
 	}
 
